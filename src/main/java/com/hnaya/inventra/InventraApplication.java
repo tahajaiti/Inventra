60b1f28c1bda48ec0443613939af144dd789f9c2
tahajaiti <taha.jaiti@gmail.com>
2025-12-23 14:29:35 +0100

    INIT: INV-17 initialized the sprting project

---
+++ src/main/java/com/hnaya/inventra/InventraApplication.java
@@ -0,0 +1,13 @@
+package com.hnaya.inventra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnaya/inventra/InventraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnaya.inventra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
