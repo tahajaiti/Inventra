dd0ba1d5c30c0664cffbceed51d8cde77f5d0b2b
YacineMARZOUK <yacinemarzou@gmail.com>
2025-12-25 12:21:21 +0100

    create the Product mapper

--- src/main/java/com/hnaya/inventra/dto/response/StockResponseDTO.java
+++ src/main/java/com/hnaya/inventra/dto/response/StockResponseDTO.java
@@ -1,4 +1,12 @@
 package com.hnaya.inventra.dto.response;
 
+import lombok.Data;
+
+@Data
 public class StockResponseDTO {
+    private Long id;
+    private ProductResponseDTO product;
+    private Long warehouseId;
+    private Integer quantiteDisponible;
+    private Integer seuilAlerte;
 }
