94d3ff26db433579e782b013e90f917adc289a96
YacineMARZOUK <yacinemarzou@gmail.com>
2025-12-29 00:28:32 +0100

    add the product controller

---
+++ src/main/java/com/hnaya/inventra/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.hnaya.inventra.controller;
+
+import com.hnaya.inventra.dto.request.ProductRequestDTO;
+import com.hnaya.inventra.dto.response.ProductResponseDTO;
+import com.hnaya.inventra.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+
+     //Réservé à l'ADMIN. Calcule automatiquement la marge en interne.
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> create(@Valid @RequestBody ProductRequestDTO requestDTO) {
+        return new ResponseEntity<>(productService.create(requestDTO), HttpStatus.CREATED);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAll() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    // Réservé à l'ADMIN.
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
