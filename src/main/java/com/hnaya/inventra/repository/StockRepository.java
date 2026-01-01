338249789d841a03ab9c7bd95eebff8ef15ff332
tahajaiti <taha.jaiti@gmail.com>
2026-01-01 19:28:41 +0100

    FIX: add find by warehouse id in repositry

--- src/main/java/com/hnaya/inventra/repository/StockRepository.java
+++ src/main/java/com/hnaya/inventra/repository/StockRepository.java
@@ -4,9 +4,12 @@ import com.hnaya.inventra.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
     Optional<Stock> findByProductIdAndWarehouseId(Long productId, Long warehouseId);
+    List<Stock> findByWarehouseId(Long warehouseId);
 }
+
